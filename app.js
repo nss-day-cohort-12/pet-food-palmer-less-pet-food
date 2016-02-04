aa09e7df09d13594372710fde5d4382759545d2c
Thomas Buida <thomasbuida@gmail.com>
2016-02-04 14:51:58 -0600

    Removed extra whitespace/ fixed indentation/ added some more comments

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-//get reference to cat and dog divs in DOM
+//get references to cat and dog divs in DOM
 var dogEl = document.getElementById('dog');
 var catEl = document.getElementById('cat');
 
-//get reference to cat and dog photo links
+//get references to cat and dog photo links
 var dogLink = document.getElementById('link-dog');
 var catLink = document.getElementById('link-cat');
 
 
 
 dogLink.addEventListener("click", function() {
+  //show dog div
   dogEl.classList.add('visible');
   dogEl.classList.remove('hidden');
 
+  //hide cat div
   catEl.classList.add('hidden');
   catEl.classList.remove('visible');
 
+  //focus on dog link, blur cat link
   dogLink.classList.add('active');
   catLink.classList.remove('active');
-})
+});
 
 
 catLink.addEventListener("click", function() {
+  //show cat div
   catEl.classList.add('visible');
   catEl.classList.remove('hidden');
 
+  //hide dog dig
   dogEl.classList.add('hidden');
   dogEl.classList.remove('visible');
 
+  //focus on cat link, blur dog link
   catLink.classList.add('active');
   dogLink.classList.remove('active');
-})
+});
